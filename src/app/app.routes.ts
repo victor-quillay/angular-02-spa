4235c7397d7c872be3599c33783bf40d80d414bd
vmquillay <grupoalpeses@gmail.com>
2021-01-12 17:12:14 -0500

    se agrega los estilos de animacion, se agrega main-container al index html, se agrega las rutas para los otros componentes se agrega el router link y roterlinkactive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { RouterModule, Routes} from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 import { HomeComponent } from './components/home/home.component';
 
 const APP_ROUTES: Routes = [
     {
         path: 'home',
         component: HomeComponent
+    },
+    {
+        path: 'heroes',
+        component: HeroesComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
     },
     {
         path: '**',
